bdc5801c823c1145dc050444635f9ffbc8283011
Vishal <vishal.n@mobtexting.com>
2017-12-30 11:57:15 +0530

    added popup

---
+++ js/popup.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function(){
+	var checkpageclick = document.getElementById("checkpage");
+	checkpageclick.addEventListener("click", function(){
+		alert("Hello world");
+	});
+});

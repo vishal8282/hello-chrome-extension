59f391587fd2b7834a91e5673cd48b3f6d47fdef
Vishal <vishal.n@mobtexting.com>
2018-01-22 18:54:02 +0530

    Added New Features

---
+++ js/options.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){	
+	restore_options();
+	$("#checkpage").click(function(){
+		save_options();
+	});
+});
+
+// Saves options to chrome.storage
+function save_options() {
+  var username = $('#username').val();
+  var api = $('#api').val();
+  chrome.storage.sync.set({
+    username: username,
+    api: api
+  }, function() {
+	api = get_hidden(api);
+    $('#status').show();
+	$('#status').text("Changes saved. Please refresh!");
+  });
+}
+
+function restore_options() {
+  chrome.storage.sync.get({
+    username: 'N/A',
+    api: 'N/A'
+  }, function(items) {
+	$('#username-text').text(items.username);
+	api = get_hidden(items.api);
+	$('#api-text').text(api);
+	display_credits(items.api);
+  });
+}
+
+function get_hidden(api){
+	var len = api.length;
+	len -= 5;
+	api = api.substring(0, 5);
+	for(var i = 0; i < len; i++){
+		api += 'X';
+	}
+	return api;
+}
+
+function display_credits(api_key){
+	$.ajax({
+		'type': 'GET',
+		'url': 'http://api.mobtexting.com/v1/credit',
+        'data': {
+        	'api_key': api_key
+        },
+        'success': function(data){ 
+        	$('#credits-text').text(data.balance);
+        },
+        'error': function (data) {
+        	$('#credits-text').text("N/A");
+	    }
+	});
+}
--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,76 @@
-document.addEventListener('DOMContentLoaded', function(){
-	var checkpageclick = document.getElementById("checkpage");
-	checkpageclick.addEventListener("click", function(){
-		alert("Hello world");
+$(document).ready(function(){	
+    $('#wait').hide();
+	$("#sendsms").click(function(){
+
+        $('#status').text('');
+        $('#wait').show();
+		var phone = $('#phone').val();
+        var message = $('#message').val();
+
+		if(!validate_phone(phone)){
+            $('#status').text('Please enter a valid phone number');
+            $('#wait').hide();
+			return;
+		}
+
+        if(!message){
+            $('#status').text('Please enter message');
+            $('#wait').hide();
+            return;
+        }
+
+        $(this).prop('disabled', true);
+
+        chrome.storage.sync.get({
+            api: 'xxxxxx'
+        }, function(items) {
+            send_sms(phone, message, items.api);
+        });
+
 	});
+
+    $("#options").click(function(){
+        var options_url = chrome.extension.getURL("../options.html"); 
+        window.open(options_url, '_blank');
+    });
 });
+
+function validate_phone(phone) {  
+
+	if(phone.length < 10){
+		return false;
+	}
+
+	// TODO : validate
+
+	return true;
+}
+
+function send_sms(phone, message, api_key){
+        $.ajax({
+                'url': 'https://mobtexting.com/app/index.php/api',
+                'type': 'GET',
+                'dataType': 'json',
+                'data': {
+                    'method': 'sms.normal',
+                    'api_key': api_key,
+                    'to': phone,
+                    'sender': 'BULKSMS',
+                    'message': message,
+                    'flash': '0',
+                    'unicode': '1'  
+                },
+                'success': function (data) {
+                    // what happens if the request was completed properly
+                    $('#status').text('SMS Sent Successfully');
+                    $('#wait').hide();
+                    $('#sendsms').prop('disabled', false);
+                },
+                'error': function (data) {
+                    // what happens if the request fails.
+                    $('#status').text('SMS Sending Failed!');
+                    $('#wait').hide();
+                    $('#sendsms').prop('disabled', false);
+                }
+            });
+    }
